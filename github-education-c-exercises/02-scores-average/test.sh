90814d07c55b8e96d47fa070cfb49ebd9072944f
minur1907 <zuber.minur@ibu.edu.mk>
2026-03-31 06:31:29 +0200

    Update test.sh

--- github-education-c-exercises/02-scores-average/test.sh
+++ github-education-c-exercises/02-scores-average/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-clang -std=c11 -Wall -Wextra -Werror scores.c -o scores
+clang -std=c11 -Wall -Wextra -Werror scores.c -o scores -lcs50
 # example run (student can compare)
 echo -e "72\n73\n33\n" | ./scores
