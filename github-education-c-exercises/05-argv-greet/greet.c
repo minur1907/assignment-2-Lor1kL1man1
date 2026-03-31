a186161f81a082290b74b75bdefb8de5c4ece4ca
minur1907 <zuber.minur@ibu.edu.mk>
2026-03-31 06:37:06 +0200

    Update greet.c

--- github-education-c-exercises/05-argv-greet/greet.c
+++ github-education-c-exercises/05-argv-greet/greet.c
@@ -5,6 +5,14 @@ int main(int argc, string argv[])
 {
     // TODO:
     // if argc == 2 print hello, argv[1]
+    if (argc == 2)
+    {
+        printf("hello, %s\n", argv[1]);
+    }
     // else print hello, world
+    else
+    {
+        printf("hello, world\n");
+    }
     return 0;
 }
